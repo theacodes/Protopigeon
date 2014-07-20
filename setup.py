a72c83c1955f227e71658a404f71a383e6e80338
Jon Wayne Parrott <jjramone13@gmail.com>
2014-07-19 20:59:02 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+VERSION = '1.0.0'
+
+setup(
+    name="protopigeon",
+    version=VERSION,
+    author='Jon Parrott',
+    author_email='jjramone13@gmail.com',
+    maintainer='Jon Parrott / Cloud Sherpas',
+    maintainer_email='jonathan.parrott@cloudsherpas.com',
+    description="A helper library for working with Google's protorpc and App Engine's datastore",
+    url='https://bitbucket.org/jonparrott/protopigeon',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    packages=['ferris3'],
+    install_requires=[
+    ],
+)
