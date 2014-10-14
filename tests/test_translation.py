e73dc14b9ef980cfcbb99c74dfa6392f5d8a6649
Jon Wayne Parrott <jjramone13@gmail.com>
2014-10-14 14:51:40 -0400

    Camelcase in tests

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -11,29 +11,29 @@ class InnerModel(ndb.Model):
 
 class MessageModelTest(ndb.Model):
     string = ndb.StringProperty()
-    repeated_string = ndb.StringProperty(repeated=True)
+    repeatedString = ndb.StringProperty(repeated=True)
     text = ndb.TextProperty()
-    repeated_text = ndb.TextProperty(repeated=True)
+    repeatedText = ndb.TextProperty(repeated=True)
     blob = ndb.BlobProperty()
-    repeated_blob = ndb.BlobProperty(repeated=True)
-    key_prop = ndb.KeyProperty()
-    repeated_key = ndb.KeyProperty(repeated=True)
+    repeatedBlob = ndb.BlobProperty(repeated=True)
+    keyProp = ndb.KeyProperty()
+    repeatedKey = ndb.KeyProperty(repeated=True)
     boolean = ndb.BooleanProperty()
-    repeated_boolean = ndb.BooleanProperty(repeated=True)
+    repeatedBoolean = ndb.BooleanProperty(repeated=True)
     integer = ndb.IntegerProperty()
-    repeated_integer = ndb.IntegerProperty(repeated=True)
+    repeatedInteger = ndb.IntegerProperty(repeated=True)
     float = ndb.FloatProperty()
-    repeated_float = ndb.FloatProperty(repeated=True)
+    repeatedFloat = ndb.FloatProperty(repeated=True)
     datetime = ndb.DateTimeProperty()
-    repeated_datetime = ndb.DateTimeProperty(repeated=True)
+    repeatedDatetime = ndb.DateTimeProperty(repeated=True)
     time = ndb.TimeProperty()
     date = ndb.DateProperty()
     geopt = ndb.GeoPtProperty()
-    repeated_geopt = ndb.GeoPtProperty(repeated=True)
+    repeatedGeopt = ndb.GeoPtProperty(repeated=True)
     blobkey = ndb.BlobKeyProperty()
-    repeated_blobkey = ndb.BlobKeyProperty(repeated=True)
+    repeatedBlobkey = ndb.BlobKeyProperty(repeated=True)
     structured = ndb.StructuredProperty(InnerModel)
-    repeated_structured = ndb.StructuredProperty(InnerModel, repeated=True)
+    repeatedStructured = ndb.StructuredProperty(InnerModel, repeated=True)
 
 
 class TestTranslation(AppEngineTest):
@@ -51,29 +51,29 @@ class TestTranslation(AppEngineTest):
 
         widget = MessageModelTest(
             string='a',
-            repeated_string=['a', 'b', 'c'],
-            key_prop=ndb.Key('Moew', 'Test'),
-            repeated_key=[ndb.Key('One', 'Cat'), ndb.Key('Two', 'Cat')],
+            repeatedString=['a', 'b', 'c'],
+            keyProp=ndb.Key('Moew', 'Test'),
+            repeatedKey=[ndb.Key('One', 'Cat'), ndb.Key('Two', 'Cat')],
             text='a',
-            repeated_text=['a', 'b', 'c'],
+            repeatedText=['a', 'b', 'c'],
             blob='abc',
-            repeated_blob=['abc', 'abc', '123'],
+            repeatedBlob=['abc', 'abc', '123'],
             boolean=True,
-            repeated_boolean=[False, True, False],
+            repeatedBoolean=[False, True, False],
             integer=5,
-            repeated_integer=[1, 2, 3, 4, 5],
+            repeatedInteger=[1, 2, 3, 4, 5],
             float=3.14,
-            repeated_float=[3.14, 1.23, 10.4],
+            repeatedFloat=[3.14, 1.23, 10.4],
             datetime=datetime.datetime.utcnow(),
             date=datetime.date.today(),
             time=datetime.datetime.utcnow().time(),
-            repeated_datetime=[datetime.datetime.utcnow(), datetime.datetime.now()],
+            repeatedDatetime=[datetime.datetime.utcnow(), datetime.datetime.now()],
             geopt=ndb.GeoPt(5, 5),
-            repeated_geopt=[ndb.GeoPt(5, 7), ndb.GeoPt(7, 8)],
+            repeatedGeopt=[ndb.GeoPt(5, 7), ndb.GeoPt(7, 8)],
             blobkey=ndb.BlobKey('oEFRyChdYLJbRk6cKXuniZfFtHct1wzDcnvVSgay91N7SoOCWTAWbDU8YcwQQbdn'),
-            repeated_blobkey=[ndb.BlobKey('oEFRyChdYLJbRk6cKXuniZfFtHct1wzDcnvVSgay91N7SoOCWTAWbDU8YcwQQbdn'), ndb.BlobKey('vQHMoSU5zK2zBxMA_fcP7A==')],
+            repeatedBlobkey=[ndb.BlobKey('oEFRyChdYLJbRk6cKXuniZfFtHct1wzDcnvVSgay91N7SoOCWTAWbDU8YcwQQbdn'), ndb.BlobKey('vQHMoSU5zK2zBxMA_fcP7A==')],
             structured=InnerModel(one='One', two=2),
-            repeated_structured=[InnerModel(one='One', two=2), InnerModel(one='Name', two=1)]
+            repeatedStructured=[InnerModel(one='One', two=2), InnerModel(one='Name', two=1)]
         )
 
         widget.put()
@@ -86,36 +86,36 @@ class TestTranslation(AppEngineTest):
         message = protopigeon.to_message(widget, WidgetMessage)
 
         assert message.string == widget.string
-        assert message.repeated_string == widget.repeated_string
+        assert message.repeatedString == widget.repeatedString
         assert message.text == widget.text
-        assert message.repeated_text == widget.repeated_text
+        assert message.repeatedText == widget.repeatedText
         assert message.blob == widget.blob
-        assert message.repeated_blob == widget.repeated_blob
+        assert message.repeatedBlob == widget.repeatedBlob
         assert message.boolean == widget.boolean
-        assert message.repeated_boolean == widget.repeated_boolean
+        assert message.repeatedBoolean == widget.repeatedBoolean
         assert message.integer == widget.integer
-        assert message.repeated_integer == widget.repeated_integer
+        assert message.repeatedInteger == widget.repeatedInteger
         assert message.float == widget.float
-        assert message.repeated_float == widget.repeated_float
-        assert message.key_prop == widget.key_prop.urlsafe()
-        assert len(message.repeated_key) == 2
-        assert message.repeated_key[0] == widget.repeated_key[0].urlsafe()
+        assert message.repeatedFloat == widget.repeatedFloat
+        assert message.keyProp == widget.keyProp.urlsafe()
+        assert len(message.repeatedKey) == 2
+        assert message.repeatedKey[0] == widget.repeatedKey[0].urlsafe()
         assert message.datetime.year == widget.datetime.year
         assert message.datetime.month == widget.datetime.month
         assert message.datetime.day == widget.datetime.day
         assert message.datetime.hour == widget.datetime.hour
         assert message.datetime.minute == widget.datetime.minute
-        assert len(message.repeated_datetime) == 2
-        assert message.repeated_datetime[0].year == widget.repeated_datetime[0].year
+        assert len(message.repeatedDatetime) == 2
+        assert message.repeatedDatetime[0].year == widget.repeatedDatetime[0].year
         assert message.geopt.lat == widget.geopt.lat
         assert message.geopt.lon == widget.geopt.lon
-        assert len(message.repeated_geopt) == 2
-        assert message.repeated_geopt[0].lat == widget.repeated_geopt[0].lat
+        assert len(message.repeatedGeopt) == 2
+        assert message.repeatedGeopt[0].lat == widget.repeatedGeopt[0].lat
         assert message.blobkey == str(widget.blobkey)
-        assert message.repeated_blobkey[1] == str(widget.repeated_blobkey[1])
+        assert message.repeatedBlobkey[1] == str(widget.repeatedBlobkey[1])
         assert message.structured.one == widget.structured.one
         assert message.structured.two == widget.structured.two
-        assert message.repeated_structured[0].one == widget.repeated_structured[0].one
+        assert message.repeatedStructured[0].one == widget.repeatedStructured[0].one
         assert message.itemId == widget.key.urlsafe()
 
         # Updating an existing instance
@@ -172,9 +172,9 @@ class TestTranslation(AppEngineTest):
         msg = WidgetMessage(string='meow')
         widget = protopigeon.to_entity(msg, widget, only=('string',))
 
-        assert widget.repeated_string
+        assert widget.repeatedString
         assert widget.string == 'meow'
 
         widget = protopigeon.to_entity(msg, widget)
 
-        assert not widget.repeated_string
+        assert not widget.repeatedString
