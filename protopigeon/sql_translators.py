b475036822a6b13b9d0a18e4053ce8d8edae81bd
Jon Wayne Parrott <jjramone13@gmail.com>
2014-11-19 13:54:42 -0500

    Basic translation to message

--- protopigeon/sql_translators.py
+++ protopigeon/sql_translators.py
@@ -1,8 +1,9 @@
 from .sql_converters import converters as default_converters
 from protorpc import messages
+import inspect
 
 
-def model_message(Model, only=None, exclude=None, converters=None, key_field='id'):
+def model_message(Model, only=None, exclude=None, converters=None):
     class_name = Model.__name__ + 'Message'
 
     columns = Model.__table__.columns
@@ -18,18 +19,9 @@ def model_message(Model, only=None, exclude=None, converters=None, key_field='id
         dict(default_converters.items() + converters.items())
         if converters else default_converters)
 
-    # Add in the key field.
-    # key_holder = holder()
-    # key_holder.name = 'key',
-    # key_holder._repeated = False
-    # field_dict = {
-    #     key_field: converters['Key'].to_field(Model, key_holder, 1)
-    # }
-
     field_dict = {}
 
-    # Add all other fields.
-    for count, name in enumerate(field_names, start=2):
+    for count, name in enumerate(field_names, start=1):
         column = columns[name]
         column_type = column.type.__class__.__name__
         converter = converters.get(column_type, None)
@@ -38,3 +30,50 @@ def model_message(Model, only=None, exclude=None, converters=None, key_field='id
             field_dict[name] = converter.to_field(Model, column, count)
 
     return type(class_name, (messages.Message,), field_dict)
+
+
+def to_message(entity, message, converters=None, only=None, exclude=None):
+    message_fields, entity_properties, fields = _common_fields(entity, message, only, exclude)
+
+    converters = dict(default_converters.items() + converters.items()) if converters else default_converters
+
+    columns = entity.__table__.columns
+
+    values = {}
+
+    for field in fields:
+        if field not in columns:
+            continue
+
+        property = columns[field]
+        message_field = message.field_by_name(field)
+        value = getattr(entity, field)
+
+        converter = converters[property.type.__class__.__name__]
+
+        if converter:
+            if value is not None:  # only try to convert if the value is meaningful, otherwise leave it as Falsy.
+                value = converter.to_message(entity, property, message_field, value)
+            values[field] = value
+
+    if inspect.isclass(message):
+        return message(**values)
+    else:
+        for name, value in values.iteritems():
+            setattr(message, name, value)
+        return message
+
+
+def _common_fields(entity, message, only=None, exclude=None):
+    message_fields = [x.name for x in message.all_fields()]
+    entity_properties = [x.name for x in entity.__table__.columns]
+
+    fields = set(message_fields) & set(entity_properties)
+
+    if only:
+        fields = set(only) & set(fields)
+
+    if exclude:
+        fields = [x for x in fields if x not in exclude]
+
+    return message_fields, entity_properties, fields
--- tests/test_sql_translation.py
+++ tests/test_sql_translation.py
@@ -12,14 +12,25 @@ Base = declarative_base()
 class Example(Base):
     __tablename__ = 'example'
     id = Column(Integer, primary_key=True)
-    name = Column(Unicode)
+    name = Column(Unicode(255))
 
 
 class TestSqlTranslations(AppEngineTest):
-    def test_sanity(self):
+    def test_model_message(self):
         Msg = sql_translators.model_message(Example)
 
         fields = dir(Msg)
 
         for column in Example.__table__.columns:
             assert column.name in fields
+
+    def test_to_message(self):
+        Msg = sql_translators.model_message(Example)
+        item = Example()
+        item.id = 12
+        item.name = 'Peter Capaldi'
+
+        msg = sql_translators.to_message(item, Msg)
+
+        assert msg.id == item.id
+        assert msg.name == item.name
